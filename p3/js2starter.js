ec890e760b0826c5202d4155bf86205656633e91
scottviteri <scottviteri@gmail.com>
2015-11-07 14:00:23 -0500

    problems

---
+++ p3/js2starter.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    var numLevels = 5; // Number of levels
+    var currentLevel = 5; // Parachute starts at the highest level
+    var parachute = $("#parachute");
+    // YOUR CODE HERE
+})
